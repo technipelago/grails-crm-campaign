7bad2502435ef14d3474564d409abb19a4303c18
Goran Ehrsson <goran@technipelago.se>
2013-09-07 23:10:26 +0200

    Upgraded project to Grails 2.2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,36 +1,41 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 
 grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
+    inherits("global") {}
     log "warn"
+    legacyResolve false
     repositories {
         grailsHome()
         mavenRepo "http://labs.technipelago.se/repo/plugins-releases-local/"
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
         grailsCentral()
+        mavenCentral()
     }
     dependencies {
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
         build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
-        test(":spock:0.7") { export = false }
-        test(":codenarc:0.18.1") { export = false }
+        runtime ":hibernate:$grailsVersion"
 
-        compile "grails.crm:crm-core:latest.integration"
+        test(":spock:0.7") {
+            export = false
+            exclude "spock-grails-support"
+        }
+        test(":codenarc:0.18.1") { export = false }
+        test(":code-coverage:1.2.6") { export = false }
+
         compile ":sequence-generator:latest.integration"
+        compile "grails.crm:crm-core:latest.integration"
+        compile "grails.crm:crm-content:latest.integration"
+        runtime "grails.crm:crm-tags:latest.integration"
+        runtime ":selection:latest.integration"
     }
 }
