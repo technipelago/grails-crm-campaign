c3e54a506ae075eede14a1503421b044d758b50c
Goran Ehrsson <goran@technipelago.se>
2014-05-18 22:23:24 +0200

    Version 1.2.7

--- CrmCampaignGrailsPlugin.groovy
+++ CrmCampaignGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import grails.spring.BeanBuilder
 
 class CrmCampaignGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.6"
+    def version = "1.2.7"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmTags']
