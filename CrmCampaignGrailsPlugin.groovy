4b64ba2ec44a7dc324738c83c2e3fde80d5a670e
Goran Ehrsson <goran@technipelago.se>
2016-02-09 13:46:14 +0100

    2.4.3-SNAPSHOT

--- CrmCampaignGrailsPlugin.groovy
+++ CrmCampaignGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import grails.spring.BeanBuilder
 
 class CrmCampaignGrailsPlugin {
     def groupId = ""
-    def version = "2.4.2"
+    def version = "2.4.3-SNAPSHOT"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmTags']
